25f5b8201ce1d57018d9c9c506a53333b6682519
Blake <blake.sabbath@gmail.com>
2016-12-26 16:43:47 +0000

    update timestamp

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 var express = require("express");
 var app = express();
+var url = require("url")
+var strftime = require("strftime")
+var moment = require("moment")
 
-app.get('/',function(req,res){
-    res.send('Hello World');
+app.get('/:query',function(req,res){
+    var id = req.params.query;
+    var unix = null
+    var natural = null
+    
+    // check jika ini adalah unixtime
+    if( +id >= 0 ){
+        unix = +id
+        natural = unixToNat(unix)
+    }
+    
+    // check jika natural time
+    if(isNaN(+id) && moment(id, "MMMM D, YYYY").isValid()){
+       unix = natToUnix(id)
+       natural = unixToNat(unix)
+    }
+    
+    var jsonObj = {"unix":unix,"natural":natural}
+    res.send(JSON.stringify(jsonObj))
+    
 });
 
 app.listen(8080,function(){
-    console.log('Server Run on port 8088');
+    console.log('Server Run on port 8080');
 });
+
+function natToUnix(date) {
+    // Conver from natural date to unix timestamp
+    return moment(date, "MMMM D, YYYY").format("X");
+}
+    
+function unixToNat(unix) {
+    // Convert unix timestamp to natural date
+    return moment.unix(unix).format("MMMM D, YYYY");
+}
+
